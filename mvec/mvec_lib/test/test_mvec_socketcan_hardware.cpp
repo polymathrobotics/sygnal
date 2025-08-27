9bef35438ecbadbb26396172298647fa1e4c8a61
Zeerek Ahmad <zeerekahmad@hotmail.com>
2025-08-27 20:56:33 +0000

    Fix missing running

--- mvec/mvec_lib/test/test_mvec_socketcan_hardware.cpp
+++ mvec/mvec_lib/test/test_mvec_socketcan_hardware.cpp
@@ -133,6 +133,7 @@ TEST_CASE("MvecRelaySocketcan hardware integration test", "[hardware]")
       }
     }
 
+    running = true;
     auto relay_status_future = std::async(std::launch::async, [&]() {
       while (running) {
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
