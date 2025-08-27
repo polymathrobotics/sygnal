e925b7a196a7dcdeba01ba3fc1f8fa4f721b546f
Zeerek Ahmad <zeerekahmad@hotmail.com>
2025-08-27 20:38:15 +0000

    pre-commit

--- mvec/mvec_lib/test/test_mvec_socketcan_hardware.cpp
+++ mvec/mvec_lib/test/test_mvec_socketcan_hardware.cpp
@@ -1,11 +1,11 @@
 // Copyright (c) 2025-present Polymath Robotics, Inc. All rights reserved
 // Proprietary. Any unauthorized copying, distribution, or modification of this software is strictly prohibited.
 
+#include <atomic>
 #include <chrono>
 #include <iostream>
 #include <memory>
 #include <thread>
-#include <atomic>
 
 #include <catch2/catch.hpp>
 
