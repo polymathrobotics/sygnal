8eb88e54163fa0b6beccfce2fdf981ecc821be1d
Zeerek <zeerekahmad@hotmail.com>
2025-08-27 23:19:05 +0000

    Update copyright and add apache license file. Clean up package.xml and cmakelists.txt

--- mvec/mvec_lib/test/mvec_relay.cpp
+++ mvec/mvec_lib/test/mvec_relay.cpp
@@ -1,5 +1,18 @@
 // Copyright (c) 2025-present Polymath Robotics, Inc. All rights reserved
-// Proprietary. Any unauthorized copying, distribution, or modification of this software is strictly prohibited.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mvec_lib/mvec_relay.hpp"
 
 #include <memory>
 #include <vector>
@@ -7,7 +20,6 @@
 #include <catch2/catch.hpp>
 
 #include "mvec_lib/core/mvec_constants.hpp"
-#include "mvec_lib/mvec_relay.hpp"
 #include "socketcan_adapter/can_frame.hpp"
 
 static polymath::socketcan::CanFrame createTestFrame(uint32_t can_id, const std::vector<uint8_t> & data)
