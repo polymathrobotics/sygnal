fb26ac28dad638e6dbc97e9d32dbc34a4123a6cb
Zeerek <zeerekahmad@hotmail.com>
2026-01-21 01:51:39 +0000

    Adding in interface base from a customer repo

---
+++ sygnal_can_interface/sygnal_can_interface_lib/include/sygnal_can_interface_lib/sygnal_mcm_interface.hpp
@@ -0,0 +1,95 @@
+// Copyright (c) 2025-present Polymath Robotics, Inc. All rights reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Copyright (c) 2025-present Polymath Robotics, Inc. All rights reserved
+// Proprietary. Any unauthorized copying, distribution, or modification of this software is strictly prohibited.
+
+#ifndef SYGNAL_CAN_INTERFACE_LIB__SYGNAL_MCM_INTERFACE_HPP_
+#define SYGNAL_CAN_INTERFACE_LIB__SYGNAL_MCM_INTERFACE_HPP_
+
+#include <array>
+#include <string>
+
+#include "socketcan_adapter/can_frame.hpp"
+#include "socketcan_adapter/socketcan_adapter.hpp"
+
+namespace polymath::sygnal
+{
+
+/// @brief Enum representing the overall state of the system
+enum class SygnalSystemState : uint8_t
+{
+  FAIL_HARD = 254,
+  HUMAN_OVERRIDE = 253,
+  FAIL_OPERATIONAL_2 = 242,
+  FAIL_OPERATIONAL_1 = 241,
+  MCM_CONTROL = 1,
+  HUMAN_CONTROL = 0
+};
+
+/// @brief Get the string of the Sygnal System State
+/// @param state input SygnalSystemState
+/// @return string of the state value
+inline std::string sygnalSystemStateToString(SygnalSystemState state)
+{
+  switch (state) {
+    case SygnalSystemState::FAIL_HARD:
+      return "FAIL_HARD";
+    case SygnalSystemState::HUMAN_OVERRIDE:
+      return "HUMAN_OVERRIDE";
+    case SygnalSystemState::FAIL_OPERATIONAL_2:
+      return "FAIL_OPERATIONAL_2";
+    case SygnalSystemState::FAIL_OPERATIONAL_1:
+      return "FAIL_OPERATIONAL_1";
+    case SygnalSystemState::MCM_CONTROL:
+      return "MCM_CONTROL";
+    case SygnalSystemState::HUMAN_CONTROL:
+      return "HUMAN_CONTROL";
+    default:
+      return "UNKNOWN_STATE";
+  }
+}
+
+class SygnalMcmInterface
+{
+public:
+  SygnalMcmInterface();
+  ~SygnalMcmInterface() = default;
+
+  std::array<SygnalSystemState, 5> get_interface_states() const
+  {
+    return sygnal_interface_states_;
+  }
+
+  SygnalSystemState get_sygnal_mcm0_state() const
+  {
+    return sygnal_mcm_0_state_;
+  }
+
+  SygnalSystemState get_sygnal_mcm1_state() const
+  {
+    return sygnal_mcm_1_state_;
+  }
+
+  bool parseMcmHeartbeatFrame(const socketcan::CanFrame & frame);
+
+private:
+  SygnalSystemState sygnal_mcm_0_state_;
+  SygnalSystemState sygnal_mcm_1_state_;
+  std::array<SygnalSystemState, 5> sygnal_interface_states_;
+};
+
+}  // namespace polymath::sygnal
+
+#endif  // SYGNAL_CAN_INTERFACE_LIB__SYGNAL_MCM_INTERFACE_HPP_
---
+++ sygnal_can_interface/sygnal_can_interface_lib/src/sygnal_mcm_interface.cpp
@@ -0,0 +1,104 @@
+// Copyright (c) 2025-present Polymath Robotics, Inc. All rights reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Copyright (c) 2025-present Polymath Robotics, Inc. All rights reserved
+// Proprietary. Any unauthorized copying, distribution, or modification of this software is strictly prohibited.
+
+#include "sygnal_can_interface_lib/sygnal_mcm_interface.hpp"
+
+#include "sygnal_dbc/mcm_heartbeat.h"
+
+namespace polymath::sygnal
+{
+
+namespace
+{
+
+/// @brief Generate the crc8 checksum assuming 8 byte data with crc at data[7]
+/// @param data Data buffer to calculate the checksum for
+/// @return crc8 checksum
+uint8_t generate_crc8(uint8_t * data)
+{
+  uint8_t crc = 0x00;
+  size_t i, j;
+  // Assumes length 8 data with last bit being checksum
+  for (i = 0; i < 7; i++) {
+    crc ^= data[i];
+    for (j = 0; j < 8; j++) {
+      if ((crc & 0x80) != 0) {
+        crc = (uint8_t)((crc << 1) ^ 0x07);
+      } else {
+        crc <<= 1;
+      }
+    }
+  }
+  return crc;
+}
+
+/// @brief Check the crc8 checksum of incoming data assuming 8 byte data with crc at data[7]
+/// @param data Data buffer to check
+/// @return true if checksum matches, false otherwise
+bool check_crc8(uint8_t * data)
+{
+  return data[7] == generate_crc8(data);
+}
+
+}  // namespace
+
+SygnalMcmInterface::SygnalMcmInterface()
+: sygnal_mcm_0_state_(SygnalSystemState::FAIL_HARD)
+, sygnal_mcm_1_state_(SygnalSystemState::FAIL_HARD)
+{
+  sygnal_interface_states_.fill(SygnalSystemState::FAIL_HARD);
+}
+
+bool SygnalMcmInterface::parseMcmHeartbeatFrame(const socketcan::CanFrame & frame)
+{
+  if (frame.get_id() != MCM_HEARTBEAT_HEARTBEAT_FRAME_ID) {
+    return false;
+  }
+
+  auto frame_copy = frame.get_frame();
+
+  if (!check_crc8(reinterpret_cast<uint8_t *>(frame_copy.data))) {
+    return false;
+  }
+
+  mcm_heartbeat_heartbeat_t unpacked_heartbeat_t;
+  if (mcm_heartbeat_heartbeat_init(&unpacked_heartbeat_t) != 0) {
+    return false;
+  }
+
+  if (mcm_heartbeat_heartbeat_unpack(&unpacked_heartbeat_t, frame_copy.data, frame_copy.len) != 0) {
+    return false;
+  }
+
+  // Set the sygnal states based on subsystem_id
+  if (unpacked_heartbeat_t.subsystem_id == 0) {
+    sygnal_mcm_0_state_ = SygnalSystemState(unpacked_heartbeat_t.system_state);
+  } else if (unpacked_heartbeat_t.subsystem_id == 1) {
+    sygnal_mcm_1_state_ = SygnalSystemState(unpacked_heartbeat_t.system_state);
+  }
+
+  // Get interface states
+  sygnal_interface_states_[0] = SygnalSystemState(unpacked_heartbeat_t.interface0_state);
+  sygnal_interface_states_[1] = SygnalSystemState(unpacked_heartbeat_t.interface1_state);
+  sygnal_interface_states_[2] = SygnalSystemState(unpacked_heartbeat_t.interface2_state);
+  sygnal_interface_states_[3] = SygnalSystemState(unpacked_heartbeat_t.interface3_state);
+  sygnal_interface_states_[4] = SygnalSystemState(unpacked_heartbeat_t.interface4_state);
+
+  return true;
+}
+
+}  // namespace polymath::sygnal
